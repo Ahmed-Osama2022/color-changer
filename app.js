028a956f6a9eeb4fd24f29260f817ed533fb8e51
ahmed-osama2022 <ahmadosama.2011@gmail.com>
2023-11-23 22:56:56 +0200

    Adding the files to the repo.

---
+++ app.js
@@ -0,0 +1,25 @@
+const body = document.body;
+
+const myFavColors = [
+  "pink",
+  "yellow",
+  "green",
+  "red",
+  "blue",
+  "orange",
+  "purple",
+  "brown",
+];
+
+// Color Chnager function
+body.addEventListener("click", () => {
+  body.style.backgroundColor =
+    myFavColors[Math.floor(Math.random() * myFavColors.length)];
+});
+
+// For any key
+window.addEventListener("keyup", (e) => {
+  console.log(e);
+  body.style.backgroundColor =
+    myFavColors[Math.floor(Math.random() * myFavColors.length)];
+});
